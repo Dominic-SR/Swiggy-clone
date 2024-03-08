99c4b374740e26ed38792deee211260d2ccb1c05
dominic <dominicseduraja3@gmail.com>
2024-03-08 23:44:25 +0530

    sandit clint added

--- zomato/sanity.js
+++ zomato/sanity.js
@@ -1,2 +1,15 @@
 import sanityClient from "@sanity/client";
-import ImageUrlBuilder from "@sanity/image-url/lib/types/builder";
+import imageUrlBuilder from "@sanity/image-url/lib/types/builder";
+
+export const client = sanityClient({
+    projectId:"vuieh8zb",
+    dataset:"production",
+    apiVersion:"2023-03-10",
+    useCdn:true 
+})
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
+
+export default client;
